898bf68c2b3db45177d0f751c6594e81ea18434e
lhnhan142 <lhnhan142@gmail.com>
2026-08-24 19:23:25 +0700

    Add files via upload

---
+++ Bai2/Bai2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a, b;
+
+        Console.Write("Nhập số a: ");
+        a = double.Parse(Console.ReadLine());
+
+        Console.Write("Nhập số b: ");
+        b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("\n===== MÁY TÍNH ĐƠN GIẢN =====");
+        Console.WriteLine("MSSV: 6551071058");
+
+        Console.WriteLine("Cộng: " + (a + b));
+        Console.WriteLine("Trừ: " + (a - b));
+        Console.WriteLine("Nhân: " + (a * b));
+
+        if (b != 0)
+        {
+            Console.WriteLine("Chia: " + (a / b));
+            Console.WriteLine("Chia lấy phần nguyên: " + ((int)a / (int)b));
+            Console.WriteLine("Chia lấy phần dư: " + ((int)a % (int)b));
+        }
+        else
+        {
+            Console.WriteLine("Chia: Không thể chia cho 0");
+            Console.WriteLine("Chia lấy phần nguyên: Không thể chia cho 0");
+            Console.WriteLine("Chia lấy phần dư: Không thể chia cho 0");
+        }
+
+        Console.WriteLine("Lũy thừa a^b: " + Math.Pow(a, b));
+
+        Console.ReadKey();
+    }
+}
---
+++ Bai3/Bai3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double celsius;
+
+        Console.Write("Nhập nhiệt độ Celsius: ");
+        celsius = double.Parse(Console.ReadLine());
+
+        double fahrenheit = celsius * 9 / 5 + 32;
+        double kelvin = celsius + 273.15;
+
+        Console.WriteLine("\n===== CHUYỂN ĐỔI NHIỆT ĐỘ =====");
+        Console.WriteLine("MSSV: 6551071058");
+        Console.WriteLine("Celsius: " + celsius + " °C");
+        Console.WriteLine("Fahrenheit: " + fahrenheit + " °F");
+        Console.WriteLine("Kelvin: " + kelvin + " K");
+
+        Console.ReadKey();
+    }
+}
